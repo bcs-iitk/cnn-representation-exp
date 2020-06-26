7cfa46cf8154abc7eacf7736d18b1dd67eae2a4f
Sanket Agrawal <babasanku@users.noreply.github.com>
2020-06-26 22:34:40 +0530

    Some Updates
    Updates as discussed on 26th June, 2000 hrs.

--- cnn-obj-categories/main.js
+++ cnn-obj-categories/main.js
@@ -12,13 +12,13 @@ $(".image2").attr("src", 'animals/stim'+j+'.png');
 
 function uncheck(){
     var k =1
-    for(k = 1;k<8;k++){
-        $('#'+k).prop("value","5");
+    for(k = 1;k<9;k++){
+        $('#'+k).prop("value","0");
 		$('#'+k).tooltip('hide');
 		$('#checkbox'+k).prop("checked", false);
 		$('#rangetext'+k).text("Slide");
     }
-	$('#'+k).prop("value","5");
+	$('#'+k).prop("value","0");
 	$('#'+k).tooltip('hide');
 	$('#rangetext'+k).text("Slide");
 };
@@ -47,7 +47,7 @@ $('#4').on('input change' ,function (){
 $('#5').on('input change' ,function (){
 	$(this).tooltip('show');
 	$('#rangetext5').text("");
-	$('#checkbox15').prop("checked", false);
+	$('#checkbox5').prop("checked", false);
 });
 $('#6').on('input change' ,function (){
 	$(this).tooltip('show');
@@ -62,42 +62,52 @@ $('#7').on('input change' ,function (){
 $('#8').on('input change' ,function (){
 	$(this).tooltip('show');
 	$('#rangetext8').text("");
+	$('#checkbox8').prop("checked", false);
+});
+$('#9').on('input change' ,function (){
+	$(this).tooltip('show');
+	$('#rangetext9').text("");
 });
 
 $('#checkbox1').change(function (){
-	$('#1').prop("value", "5");
+	$('#1').prop("value", "0");
 	$('#rangetext1').text("Slide");
 	$('#1').tooltip('hide');
 });
 $('#checkbox2').change(function (){
-	$('#2').prop("value", "5");
+	$('#2').prop("value", "0");
 	$('#rangetext2').text("Slide");
 	$('#2').tooltip('hide');
 });
 $('#checkbox3').change(function (){
-	$('#3').prop("value", "5");
+	$('#3').prop("value", "0");
 	$('#rangetext3').text("Slide");
 	$('#3').tooltip('hide');
 });
 $('#checkbox4').change(function (){
-	$('#4').prop("value", "5");
+	$('#4').prop("value", "0");
 	$('#rangetext4').text("Slide");
 	$('#4').tooltip('hide');
 });
 $('#checkbox5').change(function (){
-	$('#5').prop("value", "5");
+	$('#5').prop("value", "0");
 	$('#rangetext5').text("Slide");
 	$('#5').tooltip('hide');
 });
 $('#checkbox6').change(function (){
-	$('#6').prop("value", "5");
+	$('#6').prop("value", "0");
 	$('#rangetext6').text("Slide");
 	$('#6').tooltip('hide');
 });
 $('#checkbox7').change(function (){
-	$('#7').prop("value", "5");
+	$('#7').prop("value", "0");
 	$('#rangetext7').text("Slide");
 	$('#7').tooltip('hide');
+});
+$('#checkbox8').change(function (){
+	$('#8').prop("value", "0");
+	$('#rangetext8').text("Slide");
+	$('#8').tooltip('hide');
 });
 
 $("#foo").submit(function(event){
@@ -132,7 +142,7 @@ function submitform($form){
     // Serialize the data in the form
     serializedData += $form.serialize();
 	
-	for(k=1;k<8;k++){
+	for(k=1;k<9;k++){
 		if($('#checkbox'+k).prop("checked") == false){
 			let elementname = $('#checkbox'+k).attr('name')
 			serializedData += '&'+elementname+'=can'
