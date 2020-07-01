cddc20cd8057627ef104f952299768203d94f023
hailcpy <jaditya8889@gmail.com>
2020-07-01 17:41:24 +0530

    changed main.js to prevent same combination of image in one experiment

--- cnn-obj-categories/main.js
+++ cnn-obj-categories/main.js
@@ -1,16 +1,31 @@
 var random_id;
 $(document).ready(function(){
 	random_id = Math.floor(100000000 + Math.random()*900000000);
-});
+})
 
-var i;
-var j;
+function check(array, ind){
+	for(k = 0; k < array.length; k++){
+		if((array[k][0] == ind[0] && array[k][1] == ind[1]) || (array[k][0] == ind[1] && array[k][1] == ind[0])){
+			return true;
+		}
+	}
+	return false;
+}
+var index = []; var i; var j;
 i = Math.floor(Math.random()*120)
 j = Math.floor(Math.random()*120)
-while(i==j){
-    i = Math.floor(Math.random()*120)
-    j = Math.floor(Math.random()*120);
+while(i == j){
+	j = Math.floor(Math.random()*120)
 }
+while(check(index, [i, j])){
+	i = Math.floor(Math.random()*120)
+	j = Math.floor(Math.random()*120)
+	while(i == j){
+		j = Math.floor(Math.random()*120)
+	}
+}
+index.push([i, j])
+console.log(index)
 $(".image1").attr("src", 'animals/stim'+i+'.png');
 $(".image2").attr("src", 'animals/stim'+j+'.png');
 
@@ -25,9 +40,7 @@ function uncheck(){
 	$('#'+k).prop("value","0");
 	$('#'+k).tooltip('hide');
 	$('#rangetext'+k).text("Slide");
-};
-
-
+}
 $('#1').on('input change' ,function (){
 	$(this).tooltip('show');
 	$('#rangetext1').text("");
@@ -72,7 +85,6 @@ $('#9').on('input change' ,function (){
 	$(this).tooltip('show');
 	$('#rangetext9').text("");
 });
-
 $('#checkbox1').change(function (){
 	$('#1').prop("value", "0");
 	$('#rangetext1').text("Slide");
@@ -113,23 +125,29 @@ $('#checkbox8').change(function (){
 	$('#rangetext8').text("Slide");
 	$('#8').tooltip('hide');
 });
-
 $("#foo").submit(function(event){
     var $form = $(this);
     submitform($form);
     i = Math.floor(Math.random()*120)
-    j = Math.floor(Math.random()*120);
-    while(i==j){
+	j = Math.floor(Math.random()*120)
+	while(i == j){
+		j = Math.floor(Math.random()*120)
+	}
+	while(check(index, [i, j])){
 		i = Math.floor(Math.random()*120)
-		j = Math.floor(Math.random()*120);
-    }
+		j = Math.floor(Math.random()*120)
+		while(i == j){
+			j = Math.floor(Math.random()*120)
+		}
+	}
+	index.push([i, j])
+	console.log(index)
     uncheck();
     $(".image1").attr("src","");
     $(".image2").attr("src","");
     $(".image1").attr("src", 'animals/stim'+i+'.png');
     $(".image2").attr("src", 'animals/stim'+j+'.png');
 });
-
 var request;
 function submitform($form){
     // Abort any pending request
@@ -159,23 +177,19 @@ function submitform($form){
     // Note: we disable elements AFTER the form data has been serialized.
     // Disabled form elements will not be serialized.
     $inputs.prop("disabled", true);
-
     // Fire off the request to /form.php
     request = $.ajax({
         url: "https://script.google.com/macros/s/AKfycbzc4iXyaM74aK8-JvzhUQqe1n34VUVLstSpQVsONbCkuA-ec4U/exec",
         type: "post",
         data: serializedData
     });
-
     request.always(function () {
         // Reenable the inputs
         $inputs.prop("disabled", false);
     });
-
     // Prevent default posting of form
     event.preventDefault();
-};
-
+}
 $(".next2").click(function(){
     $("body").replaceWith("<p style  = 'text-align:center; font-size: 50px;'>Thank you for taking part in the experiment! <br> <br> Your experiment ID is "+random_id+".</p>");
 })
